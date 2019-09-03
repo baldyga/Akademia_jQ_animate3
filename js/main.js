3d47da06ab7965ac5b020e033f5c2ae97ecd101b
Aleksandra <50842332+baldyga@users.noreply.github.com>
2019-09-03 16:09:13 +0200

    2 commit

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
-
-
 $('button').click(function(){
     $('.square').animate ({ marginLeft: '100px', width: '100px', height: '100px'}, 3000, function (){
-        $('.square').css({ backgroundColor: 'blue', transition: '5s'}, function(){
-        });
+        $('.square').css({ backgroundColor: 'blue', transition: '5s'});
+            
+        setTimeout(function(){ $( 'p' )
+            .css( { display: 'inline-block' } ); }, 5000); 
     });  
 });
 
